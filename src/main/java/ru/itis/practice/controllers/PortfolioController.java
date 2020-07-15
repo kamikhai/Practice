5d2b11ab23f5dc0fb2a228c458f2fc4f4eb3cade
kami_khai <kamilla-h@bk.ru>
2020-07-15 16:41:12 +0300

    add portfolio lost

---
+++ src/main/java/ru/itis/practice/controllers/PortfolioController.java
@@ -0,0 +1,19 @@
+package ru.itis.practice.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.itis.practice.services.TagService;
+
+@Controller
+@AllArgsConstructor
+public class PortfolioController {
+
+    private TagService tagService;
+
+    @GetMapping("/portfolio")
+    private String getProfile(ModelMap modelMap){
+        return "portfolio";
+    }
+}
