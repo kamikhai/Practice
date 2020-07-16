e66cfc84c1b786e1f0992a4324cbe1df282ea794
kami_khai <kamilla-h@bk.ru>
2020-07-17 00:26:08 +0300

    add tags in competefce form

--- src/main/java/ru/itis/practice/controllers/ProfileUpdateRestController.java
+++ src/main/java/ru/itis/practice/controllers/ProfileUpdateRestController.java
@@ -12,20 +12,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itis.practice.security.details.UserDetailsImpl;
+import ru.itis.practice.services.CompetenceService;
 import ru.itis.practice.services.StudentService;
+import ru.itis.practice.services.TeacherService;
 
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
 public class ProfileUpdateRestController {
     private StudentService studentService;
+    private CompetenceService competenceService;
+    private TeacherService teacherService;
 
 
     @PostMapping("/student/about")
     @PreAuthorize(value = "hasAuthority('STUDENT')")
     public ResponseEntity<String> changeAboutMe(@RequestParam("text") String text) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        studentService.updateDescription(((UserDetailsImpl)authentication.getDetails()).getUser().getId(), text);
+        studentService.updateDescription(((UserDetailsImpl) authentication.getDetails()).getUser().getId(), text);
         return ResponseEntity.ok().body("Информация успешно обновлена");
+    }
+
+    @PostMapping("/student/competence")
+    @PreAuthorize(value = "hasAuthority('TEACHER')")
+    public ResponseEntity<String> confirmCompetence(@RequestParam("student") Long student) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        competenceService.confirm(student, teacherService.findByEmail(((UserDetailsImpl) authentication.getDetails()).getUser().getEmail()));
+        return ResponseEntity.ok().body("Компетенция успешно подтверждена");
     }
 }
